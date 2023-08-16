06ea901347a249feb6bb198ea7f32102bb0330f6
CW34\Administrator <18380449080@163.com>
2023-08-16 13:00:44 +0800

    feat: 测试lerna的使用

---
+++ lerna-pro/packages/package1/src/App.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: yangfeng
+ * @Date: 2023-08-16 12:02:44
+ * @LastEditors: yangfeng
+ * @LastEditTime: 2023-08-16 12:58:09
+ * @FilePath: \lerna-pro\packages\package1\src\App.vue
+ * @Description: 
+-->
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script setup>
+import package2 from "package2";
+import HelloWorld from './components/HelloWorld.vue'
+console.log(package2())
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lerna-pro/packages/package2/lib/package2.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: yangfeng
+ * @Date: 2023-08-16 12:31:23
+ * @LastEditors: yangfeng
+ * @LastEditTime: 2023-08-16 12:56:45
+ * @FilePath: \lerna-pro\packages\package2\lib\package2.js
+ * @Description: 
+ */
+'use strict';
+
+module.exports = package2;
+
+function package2() {
+  return 'Hello from package2';
+}
