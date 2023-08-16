3e3b74f510b9d8158128fa8b8be4dd446b407627
CW34\Administrator <18380449080@163.com>
2023-08-16 13:27:49 +0800

    feat: 测试yarn+workspace的使用

---
+++ yarn-workspace/packages/package2/src/main.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: yangfeng
+ * @Date: 2023-08-16 13:10:02
+ * @LastEditors: yangfeng
+ * @LastEditTime: 2023-08-16 13:24:15
+ * @FilePath: \yarn-workspace\packages\package2\src\main.js
+ * @Description: 
+ */
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
---
+++ yarn-workspace/packages/package2/test.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: yangfeng
+ * @Date: 2023-08-16 13:25:08
+ * @LastEditors: yangfeng
+ * @LastEditTime: 2023-08-16 13:26:12
+ * @FilePath: \yarn-workspace\packages\package2\test.js
+ * @Description: 
+ */
+export const test = {
+  title:"name"
+}
